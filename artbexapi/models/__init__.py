146655f5cdfcbdc3da124ec8fb08b1e0f0a61ace
Lauren Hanson <lohanson33@gmail.com>
2023-05-04 10:56:34 -0700

    working on dnd

--- artbexapi/models/__init__.py
+++ artbexapi/models/__init__.py
@@ -3,7 +3,7 @@ from .audience import Audience
 from .creator import Creator
 from .format import Format
 from .production import Production
-from .tone import Tone 
+from .tone import Tone
 from .artbex_audience import ArtbexAudience
 from .artbex_format import ArtbexFormat
 from .artbex_production import ArtbexProduction
