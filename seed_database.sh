962bc2e4b76b1c2c5c30d61c47a929504c712ac6
Lauren Hanson <lohanson33@gmail.com>
2023-05-08 15:33:50 -0700

    get/list/create/destory for artbex

--- seed_database.sh
+++ seed_database.sh
@@ -5,7 +5,7 @@ rm -rf ./artbexapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations artbexapi
 python3 manage.py migrate artbexapi
-python3 manage.py loaddata creator
+# python3 manage.py loaddata creator
 python3 manage.py loaddata tone
 python3 manage.py loaddata audience
 python3 manage.py loaddata format
