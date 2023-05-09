598f00cd384988ce7401cc6ae154b6b5ed25b146
Lauren Hanson <lohanson33@gmail.com>
2023-05-08 18:13:50 -0700

    images filter by category

--- artbexapi/views/image_view.py
+++ artbexapi/views/image_view.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from artbexapi.models import Image
+from artbexapi.models import Image, Category
 
 
 class ImageView(ViewSet):
@@ -13,14 +13,32 @@ class ImageView(ViewSet):
         return Response(serializer.data)
 
     def list(self, request):
-        images = Image.objects.all()
+
+        images = []
+
+        if "category" in request.query_params:
+            image_category = request.query_params['category']
+            images = Image.objects.filter(category=image_category)
+
+        # images = Image.objects.all()
         serializer = ImageSerializer(images, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ArtBexCategorySerializer(serializers.ModelSerializer):
+    """JSON serializer for category
+    """
+    class Meta:
+        model = Category
+        fields = ('id', 'category')
 
 
 class ImageSerializer(serializers.ModelSerializer):
-    """JSON serializer for image
+    """JSON serializer for images
     """
+
+    category = ArtBexCategorySerializer()
+
     class Meta:
         model = Image
         fields = ('id', 'type', 'imageUrl', 'category', )
