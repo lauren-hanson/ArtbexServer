aca53d98be2cde2902f9f4d18f6b13263b8cdbd5
Lauren Hanson <lohanson33@gmail.com>
2023-05-08 17:18:53 -0700

    changing the database so all images are hosted together

--- artbex/urls.py
+++ artbex/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from artbexapi.views import ArtBexView, FormatView, ProductionView, ToneView, AudienceView
+from artbexapi.views import ArtBexView, ImageView, CategoryView
+# , FormatView, ProductionView, ToneView, AudienceView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'artbexs', ArtBexView, 'artbex')
-router.register(r'formats', FormatView, 'format')
-router.register(r'productions', ProductionView, 'production')
-router.register(r'tones', ToneView, 'tone')
-router.register(r'audiences', AudienceView, 'audience')
+router.register(r'images', ImageView, 'image')
+router.register(r'categories', CategoryView, 'category')
+# router.register(r'formats', FormatView, 'format')
+# router.register(r'productions', ProductionView, 'production')
+# router.register(r'tones', ToneView, 'tone')
+# router.register(r'audiences', AudienceView, 'audience')
 
 urlpatterns = [
     path('', include(router.urls))
--- artbexapi/models/tone.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Tone(models.Model):
-
-    type = models.CharField(max_length=150)
-    imageUrl = models.CharField(max_length=150, blank=True)
-
-    
