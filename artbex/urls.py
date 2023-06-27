e123e5f622e45c35d2114e7fa50be56b437c78d2
Lauren Hanson <lohanson33@gmail.com>
2023-06-26 22:12:45 -0700

    all information capturing correctly

--- artbex/urls.py
+++ artbex/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from artbexapi.views import ArtBexView, ImageView, CategoryView
+from artbexapi.views import ArtBexView, ImageView, CategoryView, ArtBexImageView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'artbexs', ArtBexView, 'artbex')
 router.register(r'images', ImageView, 'image')
-router.register(r'categories', CategoryView, 'category')
+router.register(r'categories', CategoryView, 'category'),
+router.register(r'artbeximages', ArtBexImageView, 'artbeximage')
 
 urlpatterns = [
     path('', include(router.urls))
