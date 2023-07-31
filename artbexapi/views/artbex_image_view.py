dc7683a3216579b9c9d1d0ed93f65e7e0195cae9
Lauren Hanson <lohanson33@gmail.com>
2023-07-31 15:06:09 -0500

    create method added to artbex_image

--- artbexapi/views/artbex_image_view.py
+++ artbexapi/views/artbex_image_view.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from artbexapi.models import ArtBexImage
+from artbexapi.models import ArtBexImage, ArtBex, Image
 
 
 class ArtBexImageView(ViewSet):
@@ -17,10 +17,29 @@ class ArtBexImageView(ViewSet):
         serializer = ArtBexImageSerializer(artBexImages, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        """Handle POST operations to create an ArtBexImage
+
+        Returns
+            Response -- JSON serialized ArtBexImage
+        """
+        try:
+            artbex = ArtBex.objects.get(pk=request.data['artbex_id'])
+            image = Image.objects.get(pk=request.data['image_id'])
+        except (ArtBex.DoesNotExist, Image.DoesNotExist):
+            return Response({'message': 'You sent an invalid artbex or image PK'}, status=status.HTTP_404_NOT_FOUND)
+
+        artbeximage = ArtBexImage.objects.create(
+            artbex=artbex,
+            image=image
+        )
+
+        serializer = ArtBexImageSerializer(artbeximage)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ArtBexImageSerializer(serializers.ModelSerializer):
     """JSON serializer for artBexImage
     """
     class Meta:
         model = ArtBexImage
-        fields = ('artbex', 'image',  )
+        fields = ('artbex', 'image',)
